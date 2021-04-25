5319303442834d6bd2ca92e9366b418cb18242a5
stdiu_CT <stdiu@qq.com>
2021-04-25 09:37:33 +0800

    something has changed

--- Source/HUAWEI.py
+++ Source/HUAWEI.py
@@ -1,6 +1,48 @@
-class HUAWEI():
-    def __init__(self):
-        super(HUAWEI, self).__init__()
+from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton
+import os
+from PyQt5.QtWidgets import QWidget, QApplication,QVBoxLayout,QHBoxLayout,QTabWidget
+from PyQt5.uic import loadUi
+from PyQt5.QtGui import QIcon
 
-    def Huaweicheck(self):
-        print('这是华为的核查参数')
+class HUAWEI(QWidget):
+
+    def initUI(self):
+        # mainwidget = QWidget()
+        # gridlayout = QGridLayout()
+        # pb1 = QPushButton('华为界面按钮', self)
+        # gridlayout.addWidget(pb1)
+        # mainwidget.setLayout(gridlayout)
+        # self.setCentralWidget(mainwidget)
+
+        self.btn_1 = QPushButton('1', self)
+        self.btn_2 = QPushButton('2', self)
+        self.btn_3 = QPushButton('3', self)
+        self.btn_4 = QPushButton('4', self)
+
+        left_layout = QVBoxLayout()
+        left_layout.addWidget(self.btn_1)
+        left_layout.addWidget(self.btn_2)
+        left_layout.addWidget(self.btn_3)
+        left_layout.addWidget(self.btn_4)
+        left_layout.addStretch(5)
+        left_layout.setSpacing(20)
+        left_widget = QWidget()
+        left_widget.setLayout(left_layout)
+
+        self.right_widget = QTabWidget()
+        self.right_widget.tabBar().setObjectName("mainTab")
+
+        main_layout = QHBoxLayout()
+        main_layout.addWidget(left_widget)
+        main_layout.addWidget(self.right_widget)
+        main_layout.setStretch(0, 40)
+        main_layout.setStretch(1, 200)
+        main_widget = QWidget()
+        main_widget.setLayout(main_layout)
+        self.setCentralWidget(main_widget)
+
+        self.btn_1.clicked.connect(lambda: HUAWEI.Test(self))
+    def Test(self):
+        print('打印输出测试')
+
+
--- Source/test_main.py
+++ Source/test_main.py
@@ -1,10 +1,12 @@
 import sys
 import os
-from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, qApp, QMenu
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QAction, qApp, QMenu
+from PyQt5.QtWidgets import QGridLayout, QHBoxLayout, QVBoxLayout
+from PyQt5.QtWidgets import QPushButton, QLabel
+from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.uic import loadUi
 from HUAWEI import HUAWEI
-from ZET import ZET
+from ZTE import ZET
 
 class test_main(QMainWindow):
     def __init__(self):
@@ -13,35 +15,43 @@ class test_main(QMainWindow):
 
     def initUI(self):
         self.statusBar().showMessage('准备就绪')  # 状态栏
-        self.setGeometry(300, 300, 800, 500)       # 设置起始点以及大小
+        self.setGeometry(200, 200, 1100, 700)       # 设置起始点以及大小
         self.setWindowTitle('网优运营助手 v1.3')       # 设置项目名称
         self.setWindowIcon(QIcon(r'../Icon/中国电信.png'))
-        # self.show()
+
 
         # *********************************  子菜单栏  *********************************** #
         # 文件子菜单
+        backAct = QAction(QIcon(r'../Icon/主页.png'), '主页', self)
+        backAct.setStatusTip('返回主页')
+        backAct.triggered.connect(lambda :self.mainwidget())
+
         exitAct = QAction(QIcon(r'../Icon./退出.png'), '退出（&E）', self)
         exitAct.setShortcut('Ctrl+Q')
         exitAct.setStatusTip('退出程序')
         exitAct.triggered.connect(qApp.quit)
+
         # 参数核查子菜单
-        zetAct = QAction(QIcon(r'../Icon./中兴.png'), '中兴 (&Z)', self)
+        zteAct = QAction(QIcon(r'../Icon./中兴.png'), '中兴 (&Z)', self)
         # zetAct.setShortcut('Ctrl+Z')
-        zetAct.setStatusTip('中兴参数核查')
-        # zetAct.triggered.connect(ZET.Zetcheck())
+        zteAct.setStatusTip('中兴参数核查')
+        zteAct.triggered.connect( lambda: ZET.Zetcheck(self))
 
         huaweiAct = QAction(QIcon(r'../Icon/华为.png'), '华为 (&W)', self)
         huaweiAct.setStatusTip('华为参数核查')
-        huaweiAct.triggered.connect(lambda :HUAWEI.Huaweicheck())
+        huaweiAct.triggered.connect(lambda: HUAWEI.initUI(self))
+
 
         # *********************************  菜单栏  *********************************** #
         menubar = self.menuBar()
         # 文件
         fileMenu = menubar.addMenu('文件(&F)')
+        fileMenu.addAction(backAct)
         fileMenu.addAction(exitAct)
+
         # 参数核查
         checkMenu = menubar.addMenu('核查(&C)')
-        checkMenu.addAction(zetAct)
+        checkMenu.addAction(zteAct)
         checkMenu.addAction(huaweiAct)
         # 工具
         toolMenu = menubar.addMenu('工具(&T)')
@@ -53,6 +63,27 @@ class test_main(QMainWindow):
         saveas = QMenu('保存为(&S)', self)
         fileMenu.addMenu(saveas)
 
+        # *********************************  工具栏  *********************************** #
+        toolbar = self.addToolBar('工具栏')
+        toolbar.addAction(backAct)
+        toolbar.addAction(zteAct)
+        toolbar.addAction(huaweiAct)
+        toolbar.addAction(exitAct)
+
+        self.mainwidget()  # 默认加载主页
+
+    # *********************************  主界面  *********************************** #
+    def mainwidget(self):
+        mainwidget = QWidget()
+        gridlayout = QGridLayout()
+        pb1 = QPushButton('主界面按钮', self)
+        lab1 = QLabel("主页lab", self)
+        lab1.setPixmap(QPixmap(r'../Icon/中国电信.png'))
+        gridlayout.addWidget(lab1)
+        gridlayout.addWidget(pb1)
+        mainwidget.setLayout(gridlayout)
+        self.setCentralWidget(mainwidget)
+
     # *********************************  右键菜单  *********************************** #
     def contextMenuEvent(self, event):
         cmenu = QMenu(self)
@@ -60,10 +91,6 @@ class test_main(QMainWindow):
         action = cmenu.exec_(self.mapToGlobal(event.pos()))
         if action == newAct:
             print('右键新建')
-
-
-
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
